71aa3be88cdf84c9624c5b712959b34bacf6a032
oh-conomin <ddyu_yubb@yahoo.co.jp>
2021-10-24 11:40:41 +0900

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn=document.getElementById('submit');
+
+
+btn.addEventListener('click',function(){
+ const memo =document.getElementById('text').value;
+document.getElementById('t1').textContent=memo;
+
+});
+
+
+
+
+
+
+const li =document.createElement('li');
+t1.appendChild(li);
