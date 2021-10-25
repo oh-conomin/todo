e3092c971af0f28b0f64e7ea51b3458a3fc7e043
oh-conomin <ddyu_yubb@yahoo.co.jpclear>
2021-10-25 23:45:07 +0900

    btn

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const btn=document.getElementById('submit');
 const t1=document.getElementById('t1');
-const memo=document.getElementById('text')
+const memo=document.getElementById('text');
+
 
 btn.addEventListener('click',function(event){
     event.preventDefault();
     const li =document.createElement('li'); 
-    li.textContent=text.value;
-    t1.appendChild(li);
+    li.textContent=text.value; 
+    li.innerHTML='<input type="button" id="libtn" value="ok">'; 
+    t1.appendChild(li)
     text.value="";
-
 });
 
