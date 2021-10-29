b9dc664ba82baafb3525437edc7a121076129be8
oh-conomin <ddyu_yubb@yahoo.co.jpclear>
2021-10-29 22:11:03 +0900

    end

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
-const btn=document.getElementById('submit');
-const t1=document.getElementById('t1');
+const btn = document.getElementById("submit");
+const t1 = document.getElementById("t1");
 
-
-btn.addEventListener('click',function(event){
- 
+btn.addEventListener("click", function (event) {
   event.preventDefault();
-  const memo=document.getElementById('text').value;
-  const memotwo=document.getElementById('texttwo').value;
+  const memo = document.getElementById("text").value;
+  const memotwo = document.getElementById("texttwo").value;
 
-  if(memo.length> 0 && memotwo.length>0){ 
-    const li =document.createElement('li'); 
-    const okBtn=document.createElement('button');
-    okBtn.innerText="ok";
-    
-  
-    li.textContent=memo + "    " +memotwo+  "  "; 
-     
-    
+  if (memo.length > 0 && memotwo.length > 0) {
+    const li = document.createElement("li");
+    const okBtn = document.createElement("button");
+    okBtn.innerText = "ok";
 
-   
+    li.textContent = memo + "    " + memotwo + "  ";
 
+    t1.appendChild(li);
+    li.appendChild(okBtn);
 
+    text.value = "";
+    texttwo.value = "";
 
-  
-t1.appendChild(li);
-li.appendChild(okBtn);
-
-    
-text.value="";
-//memo.value=""; 
-//memotwo.value=""; 
-
-
-
-okBtn.addEventListener('click',function(event){
-        event.preventDefault();   
-        li.remove();
-        });
-      }    
-      
+    okBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      li.remove();
+    });
+  }
 });
-
-
